2e8d0a16c55388599d6cd81ba28dfadd6fa33cb5
Cosmin-Mihai Tutunaru <ctutunaru@pitechplus.com>
2014-07-29 15:58:11 +0300

    Add setStreamHandler method.

--- Service/Airship.php
+++ Service/Airship.php
@@ -3,7 +3,18 @@
 namespace Stocarul\UrbanAirshipBundle\Service;
 
 use UrbanAirship\Airship AS Base;
+use Monolog\Handler\StreamHandler;
+use UrbanAirship\UALog;
 
 class Airship extends Base
 {
+    /**
+     * Set a stream handler for logging
+     *
+     * @param \Monolog\Handler\StreamHandler $stream
+     */
+    public function setStreamHandler(StreamHandler $stream)
+    {
+        UALog::setLogHandlers(array($stream));
+    }
 }
