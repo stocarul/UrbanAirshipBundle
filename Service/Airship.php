0102e207db66f1976f60fd71727b3cca21f0336f
Cosmin-Mihai Tutunaru <ctutunaru@pitechplus.com>
2014-08-04 18:12:10 +0300

    Allow more stream handlers for logging.

--- Service/Airship.php
+++ Service/Airship.php
@@ -14,12 +14,41 @@ use UrbanAirship\UALog;
 class Airship extends Base
 {
     /**
+     * logHandlers
+     *
+     * @var \Monolog\Handler\StreamHandler[]
+     */
+    protected $logHandlers;
+
+    /**
      * Set a stream handler for logging
+     * It removes all previews handlers
      *
      * @param \Monolog\Handler\StreamHandler $stream
      */
     public function setStreamHandler(StreamHandler $stream)
     {
-        UALog::setLogHandlers(array($stream));
+        $logHandlers = array($stream);
+        $this->updateLogHandlers();
+    }
+
+    /**
+     * Add a stream handler for logging
+     *
+     * @param StreamHandler $stream
+     */
+    public function addStreamHandler(StreamHandler $stream)
+    {
+        $logHandlers[] = $stream;
+        $this->updateLogHandlers();
+    }
+
+    /**
+     * Set the handlers to the library
+     *
+     */
+    protected function updateLogHandlers()
+    {
+        UALog::setLogHandlers($this->logHandlers);
     }
 }
